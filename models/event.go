24d6077d8edcc30c05d5883c1e3b02cf1772daef
Jitendra Suthar <jitendra.kumar@technomark.io>
2024-04-03 09:47:08 +0530

    added update and delete functionality

--- models/event.go
+++ models/event.go
@@ -66,3 +66,61 @@ func GetAllEvents() ([]Event, error) {
 
 	return events, nil
 }
+
+func GetEventById(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	var dateTime []uint8 // Temporary variable to hold dateTime value
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &dateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert []uint8 to time.Time
+	parsedTime, err := time.Parse("2006-01-02 15:04:05", string(dateTime))
+	if err != nil {
+		return nil, err
+	}
+
+	// Assign parsed time to the DateTime pointer
+	event.DateTime = &parsedTime
+
+	return &event, nil
+
+}
+
+func (event Event) Update() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+	return err
+}
+
+func (event Event) Delete() error {
+	query := "DELETE FROM events WHERE id = ?"
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.ID)
+	return err
+}
